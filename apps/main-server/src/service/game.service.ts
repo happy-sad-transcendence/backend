d7cb6f6ae40c0eb1b8375368c318f075d99bd211
seungsje <seungsje@student.42seoul.kr>
2025-06-16 13:12:35 +0900

    chore: configure plugin settings

--- apps/main-server/src/service/game.service.ts
+++ apps/main-server/src/service/game.service.ts
@@ -3,6 +3,7 @@ import { TournamentRepository } from '../repository/tournament.repository.js';
 import { UserRepository } from '../repository/user.repository.js';
 import { DatabaseConfig } from '../config/database.config.js';
 import { GameUpdateRequestDTO } from '@hst/dto';
+import { FastifyInstance } from 'fastify';
 
 export class GameService {
   private gameRepo: GameRepository;
@@ -14,6 +15,10 @@ export class GameService {
     this.gameRepo = new GameRepository(db);
     this.tournamentRepo = new TournamentRepository(db);
     this.userRepo = new UserRepository(db);
+  constructor(app: FastifyInstance) {
+    this.gameRepo = new GameRepository(app.knex);
+    this.tournamentRepo = new TournamentRepository(app.knex);
+    this.userRepo = new UserRepository(app.knex);
   }
 
   async findByGamesByTournamentId(tournamentId: number): Promise<any | null> {
